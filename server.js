e18a99a5b048d93a8bb299567424177498f9d291
dzonib <nikolabosnjak381@gmail.com>
2019-06-12 22:10:59 +0200

    added crud server

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+let items = [
+    { id: 1, name: 'item1', quantity: 123 },
+    { id: 2, name: 'item2', quantity: 4 },
+    { id: 3, name: 'item3', quantity: 87 },
+    { id: 4, name: 'item4', quantity: 59 }
+];
+
+app.get('/items/get-items', (req, res) => {
+    res.json(items);
+});
+
+app.post('/items/add-item', (req, res) => {
+    const { id, name, quantity } = req.body;
+    items = [...items, { id, name, quantity }];
+
+    res.json(items);
+});
+
+app.put('/items/update-item', (req, res) => {
+    const { id, name, quantity } = req.body;
+    items = items.map(item => {
+        if (item.id === id) {
+            return { id, name, quantity };
+        }
+
+        return item;
+    });
+
+    res.json(items);
+});
+
+app.delete('/items/remove-item', (req, res) => {
+    items = items.filter(item => item.id !== req.body.id);
+
+    res.json(items);
+});
+
+app.listen(5000, () => console.log(`server running on http://localhost:5000`));

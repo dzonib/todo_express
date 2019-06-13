62cb85ba59a22fe6e5578f3fb01358a3e3df8b0d
dzonib <nikolabosnjak381@gmail.com>
2019-06-13 08:53:10 +0200

    done

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
@@ -7,40 +7,46 @@ app.use(express.json());
 app.use(cors());
 
 let items = [
-    { id: 1, name: 'item1', quantity: 123 },
-    { id: 2, name: 'item2', quantity: 4 },
-    { id: 3, name: 'item3', quantity: 87 },
-    { id: 4, name: 'item4', quantity: 59 }
+  { id: 1, name: "item1", quantity: 123 },
+  { id: 2, name: "item2", quantity: 4 },
+  { id: 3, name: "item3", quantity: 87 },
+  { id: 4, name: "item4", quantity: 59 }
 ];
 
-app.get('/items/get-items', (req, res) => {
-    res.json(items);
+app.get("/items/get-items", (req, res) => {
+  res.json(items);
 });
 
-app.post('/items/add-item', (req, res) => {
-    const { id, name, quantity } = req.body;
-    items = [...items, { id, name, quantity }];
+app.get("/items/get-item", (req, res) => {
+  const item = items.find(item => item.id === req.body.id);
 
-    res.json(items);
+  res.json(item);
 });
 
-app.put('/items/update-item', (req, res) => {
-    const { id, name, quantity } = req.body;
-    items = items.map(item => {
-        if (item.id === id) {
-            return { id, name, quantity };
-        }
+app.post("/items/add-item", (req, res) => {
+  const { id, name, quantity } = req.body;
+  items = [...items, { id, name, quantity }];
 
-        return item;
-    });
-
-    res.json(items);
+  res.json(items);
 });
 
-app.delete('/items/remove-item', (req, res) => {
-    items = items.filter(item => item.id !== req.body.id);
+app.put("/items/update-item", (req, res) => {
+  const { id, name, quantity } = req.body;
+  items = items.map(item => {
+    if (item.id === id) {
+      return { id, name, quantity };
+    }
 
-    res.json(items);
+    return item;
+  });
+
+  res.json(items);
+});
+
+app.delete("/items/remove-item", (req, res) => {
+  items = items.filter(item => item.id !== req.body.id);
+
+  res.json(items);
 });
 
 app.listen(5000, () => console.log(`server running on http://localhost:5000`));
